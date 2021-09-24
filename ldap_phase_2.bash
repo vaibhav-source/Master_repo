bf2634abe1926052d404347ed5a9943c033745f4
vaibhav-source <vaibhavpai94@gmail.com>
2021-09-24 12:58:22 +0530

    added comment

--- ldap_phase_2.bash
+++ ldap_phase_2.bash
@@ -4,3 +4,4 @@ echo "$file_name"
 
 ldapsearch -z 10500 -x -D 'cn=shanmug,ou=mobility,dc=o2,dc=com' -b 'ou=mobility,dc=o2,dc=com' '(&(!(featureCode=VoLTE))(!(featureCode=VoWiFi))(!(featureCode=iCloudVoWiFi))(featureCode=Multi-SIM))' -w Welcome@1234567|grep -e "mdn" -e "imsi" -e "featureCode" | tr '\n' ' '| awk '{gsub(/featureCode:/,"\n")}1' |awk '{gsub(/imsi:/,",")}1'|awk '{gsub(/mdn:/,",")}1'|awk -F"," '{print $3",",$2",",$1}'|sed 's/ //g'|awk 'NR>1' >> $file_name
 echo "main branch"
+echo "completed"
